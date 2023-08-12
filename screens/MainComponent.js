5a681cf75a4ebb5f98b8eb976a908f9316d766b3
Saheed Steven Adekunle <assmokson@gmail.com>
2023-08-12 13:10:00 -0500

    Week1 Worskshop

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -5,6 +5,8 @@ import DirectoryScreen from "./DirectoryScreen";
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./HomeScreen";
+import AboutScreen from "./AboutScreen";
+import ContactScreen from "./ContactScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -22,6 +24,30 @@ const HomeNavigator = () => {
         name="Home"
         component={HomeScreen}
         options={{ title: "Home" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const AboutNavigator = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="About" component={AboutScreen} />
+    </Stack.Navigator>
+  );
+};
+
+const ContactNavigator = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen
+        name="Contact"
+        component={ContactScreen}
+        options={{ title: "Contact Us" }}
       />
     </Stack.Navigator>
   );
@@ -64,6 +90,12 @@ const Main = () => {
           name="Home"
           component={HomeNavigator}
           options={{ title: "Home" }}
+        />
+        <Drawer.Screen name="About" component={AboutNavigator} />
+        <Drawer.Screen
+          name="Contact"
+          component={ContactNavigator}
+          options={{ title: "Contact Us" }}
         />
         <Drawer.Screen
           name="Directory"
