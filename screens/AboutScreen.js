a33e17488e744a3efdeca708b9c3d5dc35a8a537
Saheed Steven Adekunle <assmokson@gmail.com>
2023-08-18 06:37:51 -0500

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { Text, ScrollView } from "react-native";
 import { Card, Avatar, ListItem } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
@@ -25,6 +26,33 @@ const AboutScreen = () => {
       </ScrollView>
     );
   };
+
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+
   return (
     <ScrollView>
       <Mission />
